2c69c3e167e94808eb422ac60deedca63d0ca9c2
Latif <latif_1995@live.no>
2025-06-29 06:41:21 +0200

    react router intsall - test

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <div>Worldwise homepage</div>;
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <div>Not Found, page does not exist</div>;
+}
+
+export default PageNotFound;
